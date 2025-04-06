947a1a835eca97a7bf021b09924094cbf3001a08
Robert Martinis <robertmartinis@hotmail.com>
2025-04-06 22:56:19 +0200

    feat: Add load data and targeted view models skeleton

---
+++ i2i_dotnet/ViewModels/TargetedViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i2i_dotnet.ViewModels
+{
+    internal class TargetedViewModel
+    {
+    }
+}
