0b45f74ef08f542d4e78ab324e9b45cf40bcf234
Robert Martinis <robertmartinis@hotmail.com>
2026-02-15 12:56:14 +0100

    Feat: add save png and as csv buttons

---
+++ i2i_dotnet/Features/TargetedTab/Views/SavePlotButtonsView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace i2i_dotnet.Features.TargetedTab.Views;
+
+public partial class SavePlotButtonsView : UserControl
+{
+    public SavePlotButtonsView()
+    {
+        InitializeComponent();
+    }
+}
